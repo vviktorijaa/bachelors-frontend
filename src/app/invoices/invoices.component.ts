edf4f2a8701d0bf09e2972c7830fcf47ffd47dfe
Viktorija Vrzhovska <viktorija-vrzovska@hotmail.com>
2023-05-14 18:31:33 +0200

    Getting Scan data from backend and listing invoices properly

--- src/app/invoices/invoices.component.ts
+++ src/app/invoices/invoices.component.ts
@@ -1,10 +1,29 @@
+import {HttpClient} from '@angular/common/http';
 import {Component} from '@angular/core';
+
+const INVOICES_API = "http://localhost:8081/invoices";
 
 @Component({
   selector: 'app-invoices',
   templateUrl: './invoices.component.html',
   styleUrls: ['./invoices.component.css']
 })
-export class InvoicesComponent  {
+export class InvoicesComponent {
+
+  invoices: any;
   p: number = 1;
+
+  constructor(private http: HttpClient) {
+  }
+
+  ngOnInit(): void {
+    this.http.get(INVOICES_API).subscribe({
+      next: (data) => {
+        this.invoices = data;
+      },
+      error: (err) => {
+        console.log(err)
+      }
+    })
+  }
 }
--- src/app/scan/scan.component.ts
+++ src/app/scan/scan.component.ts
@@ -9,8 +9,14 @@ import {HttpClient, HttpHeaders} from "@angular/common/http";
 export class ScanComponent {
 
   fileToUpload: File | null = null;
+
   form: any = {
     invoice: null
+  }
+
+  jsonResponse: any = {
+    image: null,
+    regions: null
   }
 
   constructor(private http: HttpClient) {
@@ -36,6 +42,7 @@ export class ScanComponent {
     this.http.post("http://localhost:8080/scan/invoice", formData, httpOptions).subscribe({
       next: (data) => {
         console.log(data);
+        this.jsonResponse = data;
       },
       error: (err) => {
         console.log(err);
